19531cda3deb44a79880c163808f7c5c8a91e5e8
Jere Käpyaho <jere@coniferproductions.com>
2018-04-18 20:50:40 +0300

    Added helper functions and string iteration

--- fsharp/UCDump/Program.fs
+++ fsharp/UCDump/Program.fs
@@ -8,9 +8,14 @@ let characterName c =
     let info = UnicodeInfo.GetCharInfo cp
     info.Name
 
+let characterNames s = 
+    List.map characterName
+
+let explode (s: string) = [for c in s -> c]
+
 [<EntryPoint>]
 let main argv =
-    let ch = '#'
-    printfn "%c = %s" ch (characterName ch)
-
+    let s = "F#"
+    explode s |> Seq.iter (fun ch -> printfn "%c = %s" ch (characterName ch))
+    
     0 // return an integer exit code
