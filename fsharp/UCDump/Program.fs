a45d0bffcf99a6dd42fa44924c7e16caf0a3b0c1
Jere Käpyaho <jere@coniferproductions.com>
2018-04-18 20:46:40 +0300

    Added UnicodeInformation package from NuGet

--- fsharp/UCDump/Program.fs
+++ fsharp/UCDump/Program.fs
@@ -1,8 +1,16 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.Unicode
 
-open System
+let codepoint (c: char) = int c
+
+let characterName c = 
+    let cp = codepoint c
+    let info = UnicodeInfo.GetCharInfo cp
+    info.Name
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let ch = '#'
+    printfn "%c = %s" ch (characterName ch)
+
     0 // return an integer exit code
