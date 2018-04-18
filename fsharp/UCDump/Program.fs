744206996b57f721a90c17491e71d4bd1ea13ce2
Jere Käpyaho <jere@coniferproductions.com>
2018-04-18 20:55:44 +0300

    Added octet count

--- fsharp/UCDump/Program.fs
+++ fsharp/UCDump/Program.fs
@@ -11,11 +11,23 @@ let characterName c =
 let characterNames s = 
     List.map characterName
 
+let octetCount cp =
+    match cp with
+    | c when c >= 0x000000 && c <= 0x00007f -> 1
+    | c when c >= 0x000080 && c <= 0x0007ff -> 2
+    | c when c >= 0x000800 && c <= 0x00ffff -> 3
+    | c when c >= 0x010000 && c <= 0x10ffff -> 4
+    | _ -> 0
+
+let octetCounts s =
+    let codepoints = List.map codepoint s
+    List.map octetCount codepoints
+
 let explode (s: string) = [for c in s -> c]
 
 [<EntryPoint>]
 let main argv =
-    let s = "F#"
-    explode s |> Seq.iter (fun ch -> printfn "%c = %s" ch (characterName ch))
+    let s = "I \u2764 F#"
+    explode s |> Seq.iter (fun ch -> printfn "%c = %s (%i octets)" ch (characterName ch) (octetCount (codepoint ch)))
     
     0 // return an integer exit code
