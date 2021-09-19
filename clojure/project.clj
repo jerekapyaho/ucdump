b48e13e65ccb337c2bec7b2ba3220bc86505e225
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2021-09-19 12:04:18 +0300

    Updated to use Clojure 1.10

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,9 +1,9 @@
-(defproject ucdump "0.1.0-SNAPSHOT"
+(defproject ucdump "0.2.0-SNAPSHOT"
   :description "Unicode character dump for UTF-8 encoded files"
   :url "https://github.com/jerekapyaho/ucdump"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot com.coniferproductions.ucdump
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
